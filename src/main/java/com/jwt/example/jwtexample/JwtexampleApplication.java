5e4bd3c700b12753ff6366388983ef3cdcc1131f
yoojin Lee <leeujin1029@naver.com>
2022-03-29 16:35:04 +0900

    Feat: Config BcryptPasswordEncode

--- src/main/java/com/jwt/example/jwtexample/JwtexampleApplication.java
+++ src/main/java/com/jwt/example/jwtexample/JwtexampleApplication.java
@@ -2,12 +2,17 @@ package com.jwt.example.jwtexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class JwtexampleApplication {
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JwtexampleApplication.class, args);
 	}
-
 }
