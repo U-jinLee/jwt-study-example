71894b55eed283e599bf60ad1b1eebe5becb7667
yoojin Lee <leeujin1029@naver.com>
2022-03-29 16:59:48 +0900

    Feat: Override doFilterInternal method

--- src/main/java/com/jwt/example/jwtexample/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/jwt/example/jwtexample/jwt/JwtAuthorizationFilter.java
@@ -3,6 +3,12 @@ package com.jwt.example.jwtexample.jwt;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * 시큐리티가 가진 filter 중 BasicAuthentication Filter가 존재
  * 권한이나 인증이 필요한 특정 주소를 요청 시 위 필터를 탄다.
@@ -12,6 +18,14 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 
     public JwtAuthorizationFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
+        logger.info("인증이나 권한이 필요로 하는 주소");
+    }
+
+    //인증이나 권한이 필요한 주소 요청이 있을 때 해당 필터를 타게 된다.
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        super.doFilterInternal(request, response, chain);
+        logger.info("인증이나 권한이 필요로 하는 주소");
     }
 
 }
